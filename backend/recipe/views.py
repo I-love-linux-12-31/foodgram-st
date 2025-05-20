ae31505b9eee33598a4daad9f161fa27466a2259
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-05-20 16:20:15 +0300

    Mirror improvements

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -83,7 +83,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         response = HttpResponse(shopping_list, content_type="text/plain")
         response["Content-Disposition"] = (
-            'attachment; filename="shopping_list.txt"'
+            "attachment; filename=\"shopping_list.txt\""
         )
         return response
 
