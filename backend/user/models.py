ae22fcc20b3b8cd774aba32d78c232ff20c9fbad
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-05-20 15:26:19 +0300

    Mirror improvements (requested by reviewer)

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .constants import NAME_MAX_LENGTH
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='users/', blank=True, null=True)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=NAME_MAX_LENGTH)
+    last_name = models.CharField(max_length=NAME_MAX_LENGTH)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
